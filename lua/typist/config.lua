00804bad531af75847a388ff1405bdf1226267ac
BYT0723 <twang9739@gmail.com>
2023-07-02 23:40:31 +0800

    feat: caculate size and position of window

--- lua/typist/config.lua
+++ lua/typist/config.lua
@@ -1,3 +1,5 @@
+local api = vim.api
+
 local DEFAULT_OPTS = {
 	paddingLine = 1,
 	win = {
@@ -6,15 +8,37 @@ local DEFAULT_OPTS = {
 		title_pos = "center",
 		border = "double",
 		style = "minimal",
-		row = 10.1,
-		col = 10.1,
-		width = 100,
-		height = 30,
+		row = 0.5,
+		col = 0.5,
+		width = 0.7,
+		height = 0.5,
 	},
 }
 
-local function setup(conf)
-	return vim.tbl_deep_extend("force", DEFAULT_OPTS, conf or {})
+---
+--- Set config
+---
+---@param opt {}
+---@return {}
+local function setup(opt)
+	local conf = vim.tbl_deep_extend("force", DEFAULT_OPTS, opt or {})
+
+	local gh = api.nvim_win_get_height(0)
+	local gw = api.nvim_win_get_width(0)
+	if conf.win.height <= 1 then
+		conf.win.height = math.ceil(gh * conf.win.height)
+	end
+	if conf.win.width <= 1 then
+		conf.win.width = math.ceil(gw * conf.win.width)
+	end
+	if conf.win.row <= 1 then
+		conf.win.row = math.floor((gh - conf.win.height) * conf.win.row)
+	end
+	if conf.win.col <= 1 then
+		conf.win.col = math.floor((gw - conf.win.width) * conf.win.col)
+	end
+
+	return conf
 end
 
 return {
